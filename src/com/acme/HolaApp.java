7c5c5215f4e532bfabd6b587df69bec7a5571da5
imanol <imanol@unc.com>
2022-08-10 03:25:02 -0300

    Se integro clase mensaje

--- src/com/acme/HolaApp.java
+++ src/com/acme/HolaApp.java
@@ -2,6 +2,10 @@ package com.acme;
 
 public class HolaApp {
     public static void main(String[] args) {
-        System.out.printf("Hola Mundo");
+        System.out.print("Hola Mundo");
+        Mensaje msj = new Mensaje();
+        String nombre = msj.solicitaDatos("Escribe tu texto");
+        msj.saludar(nombre);
+
     }
 }
